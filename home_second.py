0c1900fd7dbdea6e58a48cd8783dfab2ec4c68c1
viktoria-nika <161070926+viktoria-nika@users.noreply.github.com>
2024-10-16 19:58:53 +0300

    добавила перевірку

--- home_second.py
+++ home_second.py
@@ -1,13 +1,10 @@
 from collections import deque
 
-def is_palidrome(letters) -> bool:
-    deq = deque()
-    for  letter in letters:
-        if deq.pop() != deq.popleft():
-            print(" Не поліндром ")
-        else:
-            deq.pop() 
-            deq.popleft()
-        print( " Поліндром ")
-    return deq
- is_palidrome(lool)  
+def is_palidrome(input_letters) -> bool:
+    cleaned_letters = ''.join(input_letters.lower().split()) # робимо не чутливий рядок до пробілів та регістру
+    deq = deque(cleaned_letters) # створюємо чергу
+    while len(deq) > 1: # створюємо цикл
+        if deq.pop() != deq.popleft(): # задаємо умову для чи запис є паліндром
+            return  " Не поліндром "
+    return  " Поліндром "
+print(is_palidrome("L ool")) # перевіряємо вводячи текст
